afd6647af313a95c68026dfc18e5c768d26eef76
Hardik Gunjal <hardikgunjal2928@gmail.com>
2023-09-01 21:39:50 -0400

    initial commit

---
+++ src/test/java/com/example/sevice/batis/BatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sevice.batis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
